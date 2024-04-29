204215d7de4e631e98797272147a58d3a11a465c
maz-k <149887419+maz-k@users.noreply.github.com>
2024-04-29 09:16:38 +0100

    Update generatePlots.m - added fulcrum tracking

--- generatePlots.m
+++ generatePlots.m
@@ -1,82 +1,165 @@
+#**********************************
+#Name: KANE_Madeleine_VideoTracking3rdYearProj
+#Author: Madeleine Kane (UoM ID: 10819933)
+#Date edited: 16/04/2024
+#Version: 6
+#**********************************
+#Based on code from pyimagesearch.com:
+#Author: Adrian Rosebrock
+#Avaliable: https://pyimagesearch.com/2018/07/30/opencv-object-tracking/
+#***********************************
 
-%read in csv file for values
-T = readtable('tester.csv');
-%extract x and y values respectivley
-x = table2array(T(:,"x"));
-y = table2array(T(:,"y"));
-%expand x values
-neg_x = x.*-1;
-dub_x = x.*2;
-full_x = cat(1, neg_x, x, dub_x);
-%plot motion path on it's own
-figure(1)
-plot(x, y);
-grid on;
-%open up another plot, used to check fulcrum point appears correct
-figure(2)
-plot(x, y);
-xlim([0 400]);
-ylim([0 400]);
-grid on;
-hold on;
-%FIND TANGENTS TO LINE
-dy = gradient(y,x); %find the numerical derivative
-%find coordinates to draw tangents at
-[min_x, min_index] = min(x);
-min_y = y(min_index);
-min_dy = dy(min_index);
-[max_x, max_index] = max(x);
-max_y = y(max_index);
-[low_y, low_index] = min(y);
-low_x = x(low_index);
-max_dy = (max_y - low_y)/(max_x - low_x);
-%find tangent lines and plot
-min_tang_y = min_dy*full_x+(min_y - dy(min_index)*min_x);
-plot(full_x, min_tang_y);
-max_tang_y = max_dy*full_x+(max_y - max_dy*max_x);
-plot(full_x, max_tang_y);
-%plot perpendicular lines
-min_perp_dy = -1/min_dy;
-min_perp_c = min_y - min_perp_dy*min_x;
-min_perp_y = min_perp_dy*full_x+min_perp_c;
-plot(full_x, min_perp_y);
-max_perp_dy = -1/max_dy;
-max_perp_c = max_y - max_perp_dy*max_x;
-max_perp_y = max_perp_dy*full_x+max_perp_c;
-plot(full_x, max_perp_y);
-%find the perpendicular lines intersect to get the fulcrum
-fulc_x = (max_perp_c - min_perp_c)/(min_perp_dy - max_perp_dy);
-fulc_y = max_perp_dy*fulc_x+max_perp_c;
-plot(fulc_x,fulc_y,'o-','MarkerFaceColor','red','MarkerEdgeColor','red')
-xlabel("x displacment (pixels)");
-ylabel("y displacment (pixels)");
-%find the angle between the lines
-m1 = min_perp_dy;
-m2 = max_perp_dy;
-ang_dis = 180-atand((m1-m2)/(1+m1*m2));
-%get video time and forearm length for velocity calc
-vid_time = input("Enter length (in seconds) of the video clip being analysed: ");
-forearm_length = input("Enter length (in m) of the forearm of the user in the video: ");
-%generate velocities
-vect_size = size(x);
-vect_l = vect_size(1);
-step = vid_time/vect_l;
-v = zeros(vect_size);
-v(1) = 0;
-for i=2:size(x)
-    v(i) = (x(i)-x(i-1))/step;
-end
-%scale to be in metres
-forearm_pix = 0.5*(sqrt((min_x-fulc_x)^2+(min_y-fulc_y)^2)+sqrt((max_x-fulc_x)^2+(max_y-fulc_y)^2));
-scale_fact = forearm_length/forearm_pix;
-v = v.*scale_fact;
-%plot graph
-t_axis = transpose([0:1:vect_l-1]*step);
-figure(3);
-plot(t_axis,v);
-%apply a median filter to remove some of the spikes due to video processing
-plot(t_axis,medfilt1(v));
-grid on;
-hold on;
-xlabel("Time (s)");
-ylabel("End effector velocity (m/s)");
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import time
+import cv2
+
+global tracker
+global trackerFulc
+
+#create and open csv file for x,y coords
+# boost, mil, tld, csrt, med
+f = open("csrt.csv", "w")
+print("file created")
+#set headers in file
+f.write("x, y, xFulc, yFulc \n")
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str,
+	help="path to input video file")
+ap.add_argument("-t", "--tracker", type=str, default="kcf",
+	help="OpenCV object tracker type")
+args = vars(ap.parse_args())
+
+# extract the OpenCV version info
+(major, minor) = cv2.__version__.split(".")[:2]
+# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
+# function to create our object tracker
+if int(major) == 3 and int(minor) < 3:
+	tracker = cv2.Tracker_create(args["tracker"].upper())
+	trackerFulc = cv2.Tracker_create(args["tracker"].upper())
+# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
+# approrpiate object tracker constructor:
+else:
+    tracker_type=args.get("tracker")
+    if tracker_type == 'BOOSTING': #problems with background
+        tracker = cv2.legacy.TrackerBoosting_create()
+        trackerFulc = cv2.legacy.TrackerBoosting_create()
+    if tracker_type == 'MIL': #good but slow
+        tracker = cv2.TrackerMIL_create()
+        trackerFulc = cv2.TrackerMIL_create()
+    if tracker_type == 'KCF': #doesn't work with csv
+        tracker = cv2.TrackerKCF_create()
+        trackerFulc = cv2.TrackerKCF_create()
+    if tracker_type == 'TLD': #BAD
+        tracker = cv2.legacy.TrackerTLD_create()
+        trackerFulc = cv2.legacy.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW': #quick analysis and good result on tester
+        tracker = cv2.legacy.TrackerMedianFlow_create()
+        trackerFulc = cv2.legacy.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN': #needs additional files installed
+        tracker = cv2.TrackerGOTURN_create()
+        trackerFulc = cv2.TrackerGOTURN_create()
+    if tracker_type == 'MOSSE': #doesn't work with csv
+        tracker = cv2.legacy.TrackerMOSSE_create()
+        trackerFulc = cv2.legacy.TrackerMOSSE_create()
+    if tracker_type == "CSRT": #BAD
+        tracker = cv2.TrackerCSRT_create()
+        trackerFulc = cv2.TrackerCSRT_create()
+
+# initialize the bounding box coordinates of the object we are going
+# to track
+initBB = None
+initBBFulc = None
+
+# if a video path was not supplied, grab the reference to the web cam
+if not args.get("video", False):
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(1.0)
+# otherwise, grab a reference to the video file
+else:
+	vs = cv2.VideoCapture(args["video"])
+# initialize the FPS throughput estimator
+fps = None
+
+# grab the current frame, then handle if we are using a
+# VideoStream or VideoCapture object
+frame = vs.read()
+frame = frame[1] if args.get("video", False) else frame
+#resize
+frame = imutils.resize(frame, width=500)
+(H, W) = frame.shape[:2]
+# select the bounding box of the object we want to track and press ENTER or SPACE
+initBB = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+initBBFulc = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+# start OpenCV object tracker using the supplied bounding box coordinates, then start the FPS throughput estimator as well
+tracker.init(frame, initBB)
+trackerFulc.init(frame, initBBFulc)
+fps = FPS().start()
+    
+# loop over frames from the video stream
+while True:
+	# grab the current frame, then handle if we are using a
+	# VideoStream or VideoCapture object
+	frame = vs.read()
+	frame = frame[1] if args.get("video", False) else frame
+	# check to see if we have reached the end of the stream
+	if frame is None:
+		break
+	# resize the frame (so we can process it faster) and grab the
+	# frame dimensions
+	frame = imutils.resize(frame, width=500)
+	(H, W) = frame.shape[:2]
+        # check to see if we are currently tracking an object
+	if (initBB is not None) and (initBBFulc is not None):
+		# grab the new bounding box coordinates of the object
+		(success, box) = tracker.update(frame)
+		(successFulc, boxFulc) = trackerFulc.update(frame)
+		# check to see if the tracking was a success
+		if success:
+			(x, y, w, h) = [int(v) for v in box]
+			cv2.rectangle(frame, (x, y), (x + w, y + h),
+				(0, 255, 0), 2)
+		if successFulc:
+                        (xFulc, yFulc, wFulc, hFulc) = [int(v) for v in boxFulc]
+                        cv2.rectangle (frame, (xFulc, yFulc),
+                                       (xFulc + wFulc, yFulc + hFulc),(0, 255, 0), 2)
+		#add to the csv file
+		f.write(str(x) + ", " + str(y) +", " + str(xFulc) + ", " + str(yFulc) + "\n")
+		# update the FPS counter
+		fps.update()
+		fps.stop()
+		# initialize the set of information we'll be displaying on
+		# the frame
+		info = [
+			("Tracker", args["tracker"]),
+			("Success", "Yes" if success and successFulc else "No"),
+			("FPS", "{:.2f}".format(fps.fps())),
+		]
+		# loop over the info tuples and draw them on our frame
+		for (i, (k, v)) in enumerate(info):
+			text = "{}: {}".format(k, v)
+			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+# if we are using a webcam, release the pointer
+if not args.get("video", False):
+    vs.stop()
+# otherwise, release the file pointer
+else:
+    vs.release()
+# close all windows
+cv2.destroyAllWindows()
+#close the file
+f.close()
