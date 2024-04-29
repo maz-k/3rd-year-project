85686e8b85a38d538921e5c5541f22c3500cdf28
maz-k <149887419+maz-k@users.noreply.github.com>
2024-04-29 09:18:41 +0100

    Update generatePlots.m - added edit due to fulcrum estimation and changed velocity filtering

--- generatePlots.m
+++ generatePlots.m
@@ -1,165 +1,132 @@
-#**********************************
-#Name: KANE_Madeleine_VideoTracking3rdYearProj
-#Author: Madeleine Kane (UoM ID: 10819933)
-#Date edited: 16/04/2024
-#Version: 6
-#**********************************
-#Based on code from pyimagesearch.com:
-#Author: Adrian Rosebrock
-#Avaliable: https://pyimagesearch.com/2018/07/30/opencv-object-tracking/
-#***********************************
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%MOTION PLOT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%read in csv file for values
+T = readtable('demo4Combi.csv');
+%extract x and y values respectivley
+x = medfilt1(table2array(T(:,"x")));
+y = medfilt1(table2array(T(:,"y")));
+%extract fulcrum values
+f_x = medfilt1(table2array(T(:,"xFulc")));
+f_y = medfilt1(table2array(T(:,"yFulc")));
+%fulc_x = mean(f_x);
+%fulc_y = mean(f_y);
+x = x-(f_x);
+y = -(y);
+%find the curve ends
+[min_y, min_index] = min(y); %set minimum at the point where the lowest value of x occurs
+min_x = x(min_index);
+[max_y, max_index] = max(y); %set maximum at the point where the highest value of y occurs
+max_x = x(max_index);
+%expand x values
+neg_x = x.*-1;
+dub_x = x.*2;
+full_x = cat(1, neg_x, x);
+%get video time and forearm length for calcs
+vid_time = input("Enter length (in seconds) of the video clip being analysed: ");
+forearm_length = input("Enter length (in m) of the forearm of the user in the video: ");
+arm = input("Enter which arm was used (0 for right, 1 for left): ");
+%flip graph if left arm used
+if (arm==1)
+    x = -x;
+end
+%min_y = y(1);
+%min_x = x(1);
+%scale pixels into meters
+forearm_pix = 0.5*sqrt((max_x-min_x)^2+(max_y-min_y)^2);
+scale_fact = forearm_length/forearm_pix;
+fulc_x = 0;
+fulc_y = 0;
+x_m = (x.*scale_fact);
+y_m = (y.*scale_fact);
+%plot motion path on it's own
+figure(1)
+plot((x_m), (y_m));
+grid on;
+%find the curve ends
+[min_y, min_index] = min(y_m); %set minimum at the point where the lowest value of x occurs
+min_x = x_m(min_index);
+[max_y, max_index] = max(y_m); %set maximum at the point where the highest value of y occurs
+max_x = x_m(max_index);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%ANGULAR ESTIMATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%plot lines from fulcrum to maximum and minimum
+dy_upper = (fulc_y-max_y)/(fulc_x-max_x);
+x_upper_vals = (fulc_x-0.02):0.1:(max_x+0.02);
+y_upper = dy_upper*x_upper_vals+(max_y - dy_upper*max_x);
+dy_lower = (fulc_y-min_y)/(fulc_x-min_x);
+x_lower_vals = (fulc_x-0.02):0.1:(min_x+0.02);
+y_lower = dy_lower*x_lower_vals+(min_y - dy_lower*min_x);
+%find the angle between the lines
+m1 = dy_lower;
+m2 = dy_upper;
+ang_dis = 180 - atand((m1-m2)/(1+m1*m2));
+figure(2)
+%motion plot
+plot(x_m,y_m);
+hold on;
+%xlim([0 300])
+%ylim([0 300])
+grid on;
+%plot estimation lines
+plot(x_upper_vals, y_upper);
+plot(x_lower_vals, y_lower);
+%plot fulcrum
+plot(fulc_x,fulc_y,'o-','MarkerFaceColor','red','MarkerEdgeColor','red')
+xlabel("x displacment (m)");
+ylabel("y displacment (m)");
+%plot max and min
+plot(min_x*scale_fact,min_y*scale_fact,'x')
+plot(max_x*scale_fact,max_y*scale_fact,'x')
 
-# import the necessary packages
-from imutils.video import VideoStream
-from imutils.video import FPS
-import argparse
-import imutils
-import time
-import cv2
-
-global tracker
-global trackerFulc
-
-#create and open csv file for x,y coords
-# boost, mil, tld, csrt, med
-f = open("csrt.csv", "w")
-print("file created")
-#set headers in file
-f.write("x, y, xFulc, yFulc \n")
-
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str,
-	help="path to input video file")
-ap.add_argument("-t", "--tracker", type=str, default="kcf",
-	help="OpenCV object tracker type")
-args = vars(ap.parse_args())
-
-# extract the OpenCV version info
-(major, minor) = cv2.__version__.split(".")[:2]
-# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
-# function to create our object tracker
-if int(major) == 3 and int(minor) < 3:
-	tracker = cv2.Tracker_create(args["tracker"].upper())
-	trackerFulc = cv2.Tracker_create(args["tracker"].upper())
-# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
-# approrpiate object tracker constructor:
-else:
-    tracker_type=args.get("tracker")
-    if tracker_type == 'BOOSTING': #problems with background
-        tracker = cv2.legacy.TrackerBoosting_create()
-        trackerFulc = cv2.legacy.TrackerBoosting_create()
-    if tracker_type == 'MIL': #good but slow
-        tracker = cv2.TrackerMIL_create()
-        trackerFulc = cv2.TrackerMIL_create()
-    if tracker_type == 'KCF': #doesn't work with csv
-        tracker = cv2.TrackerKCF_create()
-        trackerFulc = cv2.TrackerKCF_create()
-    if tracker_type == 'TLD': #BAD
-        tracker = cv2.legacy.TrackerTLD_create()
-        trackerFulc = cv2.legacy.TrackerTLD_create()
-    if tracker_type == 'MEDIANFLOW': #quick analysis and good result on tester
-        tracker = cv2.legacy.TrackerMedianFlow_create()
-        trackerFulc = cv2.legacy.TrackerMedianFlow_create()
-    if tracker_type == 'GOTURN': #needs additional files installed
-        tracker = cv2.TrackerGOTURN_create()
-        trackerFulc = cv2.TrackerGOTURN_create()
-    if tracker_type == 'MOSSE': #doesn't work with csv
-        tracker = cv2.legacy.TrackerMOSSE_create()
-        trackerFulc = cv2.legacy.TrackerMOSSE_create()
-    if tracker_type == "CSRT": #BAD
-        tracker = cv2.TrackerCSRT_create()
-        trackerFulc = cv2.TrackerCSRT_create()
-
-# initialize the bounding box coordinates of the object we are going
-# to track
-initBB = None
-initBBFulc = None
-
-# if a video path was not supplied, grab the reference to the web cam
-if not args.get("video", False):
-	print("[INFO] starting video stream...")
-	vs = VideoStream(src=0).start()
-	time.sleep(1.0)
-# otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
-# initialize the FPS throughput estimator
-fps = None
-
-# grab the current frame, then handle if we are using a
-# VideoStream or VideoCapture object
-frame = vs.read()
-frame = frame[1] if args.get("video", False) else frame
-#resize
-frame = imutils.resize(frame, width=500)
-(H, W) = frame.shape[:2]
-# select the bounding box of the object we want to track and press ENTER or SPACE
-initBB = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
-initBBFulc = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
-# start OpenCV object tracker using the supplied bounding box coordinates, then start the FPS throughput estimator as well
-tracker.init(frame, initBB)
-trackerFulc.init(frame, initBBFulc)
-fps = FPS().start()
-    
-# loop over frames from the video stream
-while True:
-	# grab the current frame, then handle if we are using a
-	# VideoStream or VideoCapture object
-	frame = vs.read()
-	frame = frame[1] if args.get("video", False) else frame
-	# check to see if we have reached the end of the stream
-	if frame is None:
-		break
-	# resize the frame (so we can process it faster) and grab the
-	# frame dimensions
-	frame = imutils.resize(frame, width=500)
-	(H, W) = frame.shape[:2]
-        # check to see if we are currently tracking an object
-	if (initBB is not None) and (initBBFulc is not None):
-		# grab the new bounding box coordinates of the object
-		(success, box) = tracker.update(frame)
-		(successFulc, boxFulc) = trackerFulc.update(frame)
-		# check to see if the tracking was a success
-		if success:
-			(x, y, w, h) = [int(v) for v in box]
-			cv2.rectangle(frame, (x, y), (x + w, y + h),
-				(0, 255, 0), 2)
-		if successFulc:
-                        (xFulc, yFulc, wFulc, hFulc) = [int(v) for v in boxFulc]
-                        cv2.rectangle (frame, (xFulc, yFulc),
-                                       (xFulc + wFulc, yFulc + hFulc),(0, 255, 0), 2)
-		#add to the csv file
-		f.write(str(x) + ", " + str(y) +", " + str(xFulc) + ", " + str(yFulc) + "\n")
-		# update the FPS counter
-		fps.update()
-		fps.stop()
-		# initialize the set of information we'll be displaying on
-		# the frame
-		info = [
-			("Tracker", args["tracker"]),
-			("Success", "Yes" if success and successFulc else "No"),
-			("FPS", "{:.2f}".format(fps.fps())),
-		]
-		# loop over the info tuples and draw them on our frame
-		for (i, (k, v)) in enumerate(info):
-			text = "{}: {}".format(k, v)
-			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-# if we are using a webcam, release the pointer
-if not args.get("video", False):
-    vs.stop()
-# otherwise, release the file pointer
-else:
-    vs.release()
-# close all windows
-cv2.destroyAllWindows()
-#close the file
-f.close()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%VELOCITY ESTIMATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%generate velocities
+vect_size = size(x);
+vect_l = vect_size(1);
+step = vid_time/vect_l;
+v = zeros(vect_size);
+v(1) = 0;
+s_current = sqrt(x(1)^2+y(1)^2);
+for i=2:size(x)
+    s_old = s_current;
+    s_current = sqrt((x(i-1)-x(i))^2+(y(i-1)-y(i))^2);
+    y_direction = (sign(y(i)-y(i-1)));
+    x_direction = (sign(x(i)-x(i-1)));
+    if (y_direction == -1)
+        direction = -1;
+    elseif (x_direction == -1)
+        direction = -1;
+    elseif (y_direction == 1)
+        direction = 1;
+    elseif (x_direction == 1)
+        direction = 1;
+    else 
+        direction = 0;
+    end
+    v(i) = (s_current/step)*direction;
+    %if v(i)==0
+        %v(i)= v(i-1);
+    %end
+end
+%take local means
+v_mean = movmean(v, 5);
+%scale to be in metres
+v = v.*scale_fact;
+v_mean = v_mean.*scale_fact;
+%plot graph
+t_axis = transpose([0:1:vect_l-1]*step);
+figure(3);
+%apply a median filter to remove some of the spikes due to video processing
+scatter(t_axis,v);
+hold on;
+%exclude first and last 10 points due to tracker instability
+plot(t_axis(10:size(x)-5), medfilt1(v_mean(10:size(x)-5)), LineWidth = 3);
+grid on;
+ylim([-0.5 0.5]);
+xlabel("Time (s)");
+ylabel("End effector velocity (m/s)");
+legend("Unfiltered data", "Filtered data");
